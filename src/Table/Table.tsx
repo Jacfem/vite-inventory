d5ab99a8e68256a398baf105b4e96abb96016050
Jaclyn Feminella <jaclynfeminella@gmail.com>
2024-10-22 11:35:55 -0400

    Add initial react query client and move fetch to it

--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,50 +6,39 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
+import { getProducts } from "../../api/products";
+
 export default function BasicTable() {
-  const [products, setProducts] = useState([]);
-  const fetchProducts = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/api/products");
-
-      const data = await response.json();
-      console.log({ data });
-      setProducts(data);
-    } catch (error) {
-      console.error("Error fetching message:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+  const { data } = getProducts();
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Product Name</TableCell>
-            <TableCell align="right">Quantity</TableCell>
-            <TableCell align="right">Size</TableCell>
-            <TableCell align="right">Expiration Date</TableCell>
-            <TableCell align="right">Location</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {/* add types and other fields */}
-          {products?.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
+    data && (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Product Name</TableCell>
+              <TableCell align="right">Quantity</TableCell>
+              <TableCell align="right">Size</TableCell>
+              <TableCell align="right">Expiration Date</TableCell>
+              <TableCell align="right">Location</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {/* add types and other fields */}
+            {data.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    )
   );
 }
